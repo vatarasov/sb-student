cac1264b214d4c3640970686c9b89eaceb4841e2
Vladimir Tarasov <vtarasov@naumen.ru>
2019-09-22 13:59:38 +0500

    Initial commit

---
+++ src/main/java/ru/vtarasov/sb/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package ru.vtarasov.sb.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
